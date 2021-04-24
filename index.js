1f3874d4898d823ba1cb3a06625e8e23d515ed68
Maria Bruner <mariab2898@gmail.com>
2021-04-24 13:56:04 -0400

    (finished wireframe of main section - Maria) Moving on to js to update DOM

---
+++ index.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("transaction-form")
+
+form.onsubmit = (e) => {
+    console.log(e)
+    console.log(e.target["transaction-type"].value)
+    console.log(e.target.amount.value)
+
+    e.preventDefault()
+}
+
